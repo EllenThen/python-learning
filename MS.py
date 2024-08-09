428f90ed6d268f50c3bf88d3065522108c5a31b4
EllenThen <145642376+EllenThen@users.noreply.github.com>
2024-08-09 08:44:16 +0000

    modified:   MS.py new file:   first progam.py

--- MS.py
+++ MS.py
@@ -1,1 +1,12 @@
+'''
+x = input("what's your name? ")
+print ("hello" + x )
+
+
+x = int(input('Type the first number: ')) ;\
+y = int(input('Type the second number: '));\
+print('The sum is: ', x + y )
+
+'''
+
 
---
+++ first progam.py
@@ -0,0 +1,10 @@
+
+'''
+from datetime import date
+print(date.today())
+
+'''
+
+parsecs_input = int(input('Input number of parsecs:'))
+lightyears = parsecs_input * 3.26156
+print(str(parsecs_input) + ' parsecs is ' + str(lightyears) + ' lightyears ')
