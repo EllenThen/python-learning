e7180d22c3c36fa4661a81d56ed48b3ba58908eb
EllenThen <145642376+EllenThen@users.noreply.github.com>
2024-08-12 08:41:09 +0000

    modified:   first progam.py

--- first progam.py
+++ first progam.py
@@ -4,7 +4,18 @@ from datetime import date
 print(date.today())
 
 '''
-
+'''
 parsecs_input = int(input('Input number of parsecs:'))
 lightyears = parsecs_input * 3.26156
 print(str(parsecs_input) + ' parsecs is ' + str(lightyears) + ' lightyears ')
+'''
+'''
+x = int(input("object size: "))
+y = int(input("proximity: "))
+if x > 5 and y < 1000:
+    print("Evasive maneuvers required")
+else:
+    print("Object poses no threat")
+
+'''
+
